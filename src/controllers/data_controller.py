e3d26fe432a7968e0e70ee358922971e3528eca2
faviercatwit <favierc@wit.edu>
2024-07-05 11:25:08 -0400

    Added data cleaning

--- src/controllers/data_controller.py
+++ src/controllers/data_controller.py
@@ -1,7 +1,7 @@
 from flask import request, Response, json, Blueprint
 import os
 import requests
-
+import pandas as pd
 
 data = Blueprint('data', __name__)
 
@@ -13,6 +13,7 @@ def getScores():
 
         required_fields = ['ouraToken', 'startDate', 'endDate']
         if all(field in data for field in required_fields):
+            main_url = f"{os.getenv('OURA_API_BASE_URI')}/sleep" 
             sleep_url = f"{os.getenv('OURA_API_BASE_URI')}/daily_sleep"
             activity_url = f"{os.getenv('OURA_API_BASE_URI')}/daily_activity"
             readiness_url = f"{os.getenv('OURA_API_BASE_URI')}/daily_readiness"
@@ -25,16 +26,44 @@ def getScores():
             'Authorization': f"Bearer {data['ouraToken']}" 
             }
 
+            main_response = requests.get(main_url, headers=headers, params=params)
             sleep_response = requests.get(sleep_url, headers=headers, params=params)
             activity_response = requests.get(activity_url, headers=headers, params=params)
             readiness_response = requests.get(readiness_url, headers=headers, params=params)
 
-            sleep_score = sleep_response.json().get("data")[0].get("score")
-            activity_score = activity_response.json().get("data")[0].get("score")
-            readiness_score = readiness_response.json().get("data")[0].get("score")
+            df_main = pd.DataFrame(main_response.json()['data'])
+            df_sleep = pd.DataFrame(sleep_response.json()['data'])
+            df_activity = pd.DataFrame(activity_response.json()['data'])
+            df_readiness = pd.DataFrame(readiness_response.json()['data'])
+
+            df = df_main.merge(df_sleep[["contributors", "day", "score"]], on='day', how='left').merge(df_activity.rename({"score":"activity_score"}, axis=1)[["day","activity_score"]], on="day", how="left")
+
+            records = []
+            for i, row in df.iterrows():
+                records.append({
+                    "day": row["day"],
+                    "sleep_score": row["score"],
+                    "readiness_score": row["readiness"]["score"],
+                    "activity_score": row["activity_score"],
+                    "efficiency": row["efficiency"],
+                    "restfulness": row["contributors"]["restfulness"],
+                    "total_sleep": row["total_sleep_duration"],
+                    "awake": row["awake_time"],
+                    "rem_sleep": row["rem_sleep_duration"],
+                    "light_sleep": row["light_sleep_duration"],
+                    "deep_sleep": row["deep_sleep_duration"],
+                    "latency": row["latency"],
+                    "bedtime_start": row["bedtime_start"],
+                    "bedtime_end": row["bedtime_end"],
+                    "heart_rate": row["heart_rate"]["items"],
+                    "average_heart_rate": row["average_heart_rate"],
+                    "hrv": row["hrv"]["items"],
+                    "average_hrv": row["average_hrv"]
+                })
+
 
             return Response(
-                response=json.dumps({'message': "success", 'data': {'sleep': sleep_score, 'activity': activity_score, 'readiness': readiness_score}}),
+                response=json.dumps({'message': "success", 'data': records}),
                 status=200,
                 mimetype='application/json'
             )
